3c5848020a451c7c1b9fda4e7ea49180c09d13cf
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-07-17 10:17:28 +0530

    Set up gameboard module and player factory function. Add logic to update board array when field is clicked

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+const markerBtnX = document.getElementById('x')
+const markerBtnO = document.getElementById('o')
+let selectedMarker;
+
+const Player = ( playerName, marker) => {
+return {playerName, marker}
+}
+
+const Gameboard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""]
+    
+    const getBoard = () => board
+    
+    const updateBoard = (index, marker) => {
+        if (index >= 0 && index < board.length && board[index] === ""){
+            board[index] = marker
+            return true
+        }
+        return false
+    }
+    
+    const resetBoard = () => {
+        board = ["", "", "", "", "", "", "", "", ""]
+    }
+    
+    return { getBoard, updateBoard, resetBoard}
+
+})()
+
 const gameBoardDisplay = (() => {
-    const markerBtnX = document.getElementById('x')
-    const markerBtnO = document.getElementById('o')
-    let selectedMarker;
     
     markerBtnX.addEventListener('click', () => {
         selectedMarker = 'X'
@@ -21,7 +47,11 @@ const gameBoardDisplay = (() => {
     fieldList.forEach(field => {
         field.addEventListener('click', () => {
             field.textContent = selectedMarker
+            let index = field.getAttribute('data-field')
+            Gameboard.updateBoard(index, selectedMarker)
             console.log(selectedMarker)
+            console.log(Gameboard.getBoard())
         })
     })
+
 })()
