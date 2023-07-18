2f4a843e84da585eaed3958863836bc0b6d6de4c
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-07-18 10:18:39 +0530

    Add logic to display selected marker when fields are clicked

--- computerMode.js
+++ computerMode.js
@@ -1,5 +1,27 @@
 const homePageBtn = document.getElementById('homePageBtn')
+const markerBtnX = document.getElementById('x')
+const markerBtnO = document.getElementById('o')
+const fieldList = document.querySelectorAll('.field')
+let marker
 
 homePageBtn.addEventListener('click', () => {
     window.location.href = 'index.html'
+})
+
+markerBtnX.addEventListener('click', () => {
+    marker = 'X'
+    markerBtnX.classList.add('active')
+    markerBtnO.classList.remove('active')
+})
+
+markerBtnO.addEventListener('click', () => {
+    marker = 'O'
+    markerBtnO.classList.add('active')
+    markerBtnX.classList.remove('active')
+})
+
+fieldList.forEach(field => {
+    field.addEventListener('click', () => {
+        field.textContent = marker
+    })
 })
